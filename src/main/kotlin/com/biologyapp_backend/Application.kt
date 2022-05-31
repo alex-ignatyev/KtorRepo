86ec77e05ee18ab0a28fc82559b496786ea29b42
Игнатьев Алексей Анатольевич <a.ignatev@dev.vtb.ru>
2022-05-31 18:47:00 +0300

    BA-3: Рефакторинг после ревью

--- src/main/kotlin/com/biologyapp_backend/Application.kt
+++ src/main/kotlin/com/biologyapp_backend/Application.kt
@@ -1,16 +1,19 @@
 package com.biologyapp_backend
 
+import com.biologyapp_backend.feature.configureTestRouting
 import com.biologyapp_backend.feature.login.configureLoginRouting
 import com.biologyapp_backend.feature.register.configureRegisterRouting
-import com.biologyapp_backend.plugins.configureRouting
-import com.biologyapp_backend.plugins.configureSerialization
+import com.biologyapp_backend.plugins.setupSerialization
 import io.ktor.server.cio.CIO
 import io.ktor.server.engine.embeddedServer
 
+// "http://0.0.0.0:8080" - BaseUrl серверащ
+
 fun main() {
     embeddedServer(CIO, port = 8080, host = "0.0.0.0") {
-        configureSerialization()
-        configureRouting()
+        setupSerialization()
+
+        configureTestRouting()
         configureLoginRouting()
         configureRegisterRouting()
     }.start(wait = true)
--- src/main/kotlin/com/biologyapp_backend/plugins/Serialization.kt
+++ src/main/kotlin/com/biologyapp_backend/plugins/Serialization.kt
@@ -4,17 +4,13 @@ import io.ktor.serialization.kotlinx.json.json
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.plugins.contentnegotiation.ContentNegotiation
-import kotlinx.serialization.json.Json
 
 
 /**
  * Инициализация библиотеки serialization.kotlinx.json
  * */
-fun Application.configureSerialization() {
+fun Application.setupSerialization() {
     install(ContentNegotiation) {
-        json(Json {
-            prettyPrint = true
-            isLenient = true
-        })
+        json()
     }
 }
