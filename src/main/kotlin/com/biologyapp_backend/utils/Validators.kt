833fcbd02b6dfb5bda830b1478229f1db8f550bc
LAPTOP-GLDRARJU\artem <artem.kolesnichenko@joy-dev.com>
2022-05-31 19:22:46 +0300

    BA-4 добавил пароль для регистрации

--- src/main/kotlin/com/biologyapp_backend/utils/Validators.kt
+++ src/main/kotlin/com/biologyapp_backend/utils/Validators.kt
@@ -3,3 +3,11 @@ package com.biologyapp_backend.utils
 fun String.isValidEmail(): Boolean {
     return this.contains("@")
 }
+
+fun String.isValidPassword(): Boolean {
+    return this.length in 4..10
+}
+
+fun String.isValidRepeatPassword(password: String): Boolean {
+    return this == password
+}
